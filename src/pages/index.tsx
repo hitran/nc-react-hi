0ba3de8b7c9f766d5559eb0ff2fcac2f2b9a029c
Hi Tran <thuchitran@gmail.com>
2020-07-04 21:22:52 +0700

    update get product list with getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,6 @@ import Head from 'next/head'
 import Layout from '../components/Layout/Layout'
 import styled from 'styled-components'
 import Button from '../components/ui-kits/Button/Button'
-import withApollo from '../utils/withApollo'
-import { useQuery } from '@apollo/react-hooks'
-import { GET_PRODUCTS } from '../graphql/product/product.query'
 import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 import { Card } from '../components/ui-kits/Card'
@@ -20,19 +17,8 @@ export const StyledHomeBody = styled.div`
   grid-gap: 10px;
 `
 
-function Home() {
-  const { loading, error, data } = useQuery(GET_PRODUCTS, {
-    variables: {
-      input: {
-        page: 1,
-        keyword: 'Samsung',
-      },
-    },
-  })
-  if (error) return <h1>Error</h1>
-  if (loading) return <h1>Loading...</h1>
-
-  const products = data?.getAllProduct?.data
+function Home(props) {
+  const products = props.productList.data
   if (!products || !products.length) {
     return <p>Not found</p>
   }
@@ -67,4 +53,15 @@ function Home() {
   )
 }
 
-export default withApollo({ ssr: true })(Home)
+export async function getStaticProps() {
+  const res = await fetch('https://min-shop.herokuapp.com/rest/product/')
+  const productList = await res.json()
+
+  return {
+    props: {
+      productList,
+    },
+  }
+}
+
+export default Home
