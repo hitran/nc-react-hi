ba64abde98b92a8f28fb57ec62a09db74c043410
thi <thi@yahoo-corp.jp>
2020-07-09 14:43:47 +0700

    update styling for product detail

---
+++ src/pages/product/Product.styled.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const StyledProduct = styled.div`
+  margin: 70px 10px 0 10px;
+`
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,46 @@
 import React from 'react'
-import { useRouter } from 'next/router'
+import { baseUrl } from '../../common/constants';
+import { StyledProduct } from './Product.styled';
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 
-const ProductDetail = () => {
-  const router = useRouter()
-  const { id } = router.query
-  return <p>ProductDetail: {id}</p>
+export interface IProductDetail {
+  description: string
+}
+export interface PropductDetailProps {
+  product: IProductDetail
+}
+const ProductDetail: React.FC<PropductDetailProps> = (props) => {
+  return (
+    <>
+      <Header/>
+      <StyledProduct dangerouslySetInnerHTML={{ __html: props.product.description }}/>
+      <Footer/>
+    </>
+  )
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${baseUrl}/product/`)
+  const productList = await res.json()
+
+  const paths = productList.data.map((product) => ({
+    params: { id: product.id },
+  }))
+
+  return { paths, fallback: true }
+}
+
+export async function getStaticProps({params}) {
+  const { id } = params
+  const res = await fetch(`${baseUrl}/product/${id}`)
+  const product= await res.json()
+
+  return {
+    props: {
+      product,
+    },
+  }
 }
 
 export default ProductDetail
